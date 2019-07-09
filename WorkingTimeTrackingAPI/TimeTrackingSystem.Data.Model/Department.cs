9938d5e3e19c0fffb9d6d9593bbba2c466eb6bee
Nikolay Nikolaev <inqwit@gmail.com>
2019-07-09 23:05:27 +0300

    добавил модели отдела, сотрудника, рабочего времени

---
+++ WorkingTimeTrackingAPI/TimeTrackingSystem.Data.Model/Department.cs
@@ -0,0 +1,17 @@
+﻿namespace TimeTrackingSystem.Data.Model
+{
+    /// <summary>
+    /// Модель отдела
+    /// </summary>
+    public class Department
+    {
+        /// <summary>
+        /// Идентификатор отдела
+        /// </summary>
+        public long DepartmentId { get; set; }
+        /// <summary>
+        /// Наименование отдела
+        /// </summary>
+        public string DepartmentName { get; set; }
+    }
+}
---
+++ WorkingTimeTrackingAPI/TimeTrackingSystem.Data.Model/Employee.cs
@@ -0,0 +1,25 @@
+﻿namespace TimeTrackingSystem.Data.Model
+{
+    /// <summary>
+    /// Модель сотрудника
+    /// </summary>
+    public class Employee
+    {
+        /// <summary>
+        /// Идентификатор сотрудника
+        /// </summary>
+        public long EmployeeId { get; set; }
+        /// <summary>
+        /// Имя сотрудника
+        /// </summary>
+        public string FirstName { get; set; }
+        /// <summary>
+        /// Фамилия сотрудника
+        /// </summary>
+        public string LastName { get; set; }
+        /// <summary>
+        /// Идентификатор отдела сотрудника
+        /// </summary>
+        public long DepartmentId { get; set; }
+    }
+}
