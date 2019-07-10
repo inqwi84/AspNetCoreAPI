eb5ab7394aa9137d067227c00cde894d91a16f00
Nikolay Nikolaev <inqwit@gmail.com>
2019-07-10 15:04:46 +0300

    почистил контроллер

--- WorkingTimeTrackingAPI/TimeTrackingSystem/Controllers/TimeTrackingSystemController.cs
+++ WorkingTimeTrackingAPI/TimeTrackingSystem/Controllers/TimeTrackingSystemController.cs
@@ -129,30 +129,5 @@ namespace TimeTrackingSystem.Controllers
                 return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
             }
         }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        //// POST api/values
-        //[HttpPost]
-        //public void Post([FromBody] string value)
-        //{
-        //}
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
